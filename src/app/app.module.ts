94b35ea950105a350ff5c255c16e82b3dd6fd414
stoneLeaf <owi.stoneleaf@gmail.com>
2019-01-09 12:54:44 +0100

    Add basic layout with nav bar, content wrapper and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
