3485384ef52eee98633d99f4a1ab343199143334
stoneLeaf <owi.stoneleaf@gmail.com>
2019-01-13 18:41:33 +0100

    Add back-end API URL to config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Should not end with a trailing slash
+  apiUrl: 'http://localhost:3000/api/v1'
 };
 
 /*
