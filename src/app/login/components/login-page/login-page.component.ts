e53ca3510798ff32bf8021c514ca3d39c058dc84
stoneLeaf <owi.stoneleaf@gmail.com>
2019-01-14 12:03:22 +0100

    Implement login page

--- src/app/login/components/login-page/login-page.component.ts
+++ src/app/login/components/login-page/login-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/services/auth.service';
+import { ValidationError } from 'src/app/shared/errors/validation.error';
 
 @Component({
   selector: 'app-login-page',
@@ -6,8 +11,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+  submitted = false;
+  waiting = false;
+  badCredentials = false;
 
-  onSubmit() {
-    // Not implemented
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private router: Router,
+              private authService: AuthService) { }
+
+  onSubmit(loginForm: NgForm) {
+    this.badCredentials = false;
+    this.submitted = true;
+
+    if (loginForm.invalid) {
+      return;
+    }
+
+    this.waiting = true;
+
+    this.authService.logIn(this.credentials.email, this.credentials.password)
+                    .subscribe(
+                      () => { this.router.navigate(['dashboard']); },
+                      error => {
+                        if (error instanceof ValidationError) {
+                          this.waiting = false;
+                          this.submitted = false;
+                          this.badCredentials = true;
+                          loginForm.reset();
+                        }
+                    });
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
-
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
   ],
   imports: [
     CommonModule,
+    FormsModule,
     LoginRoutingModule
   ]
 })
