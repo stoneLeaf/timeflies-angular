597b7f64a41687a1548481b7343afd9b7acddb8e
stoneLeaf <owi.stoneleaf@gmail.com>
2019-01-24 19:02:19 +0100

    Add read and update to project service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 import { Project } from '../shared/models/project.model';
 import { environment } from 'src/environments/environment';
+import { NotFoundError } from '../shared/errors/not-found.error';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,20 @@ export class ProjectService {
 
   create(project: Project): Observable<Project> {
     return this.http.post(`${environment.apiUrl}/projects`, project)
+                    .pipe(map(response => {
+                      return response['project'] as Project;
+                    }));
+  }
+
+  update(project: Project): Observable<Project> {
+    return this.http.patch(`${environment.apiUrl}/projects/${project.id}`, project)
+                    .pipe(map(response => {
+                      return response['project'] as Project;
+                    }));
+  }
+
+  getById(id: string): Observable<Project> {
+    return this.http.get(`${environment.apiUrl}/projects/${id}`)
                     .pipe(map(response => {
                       return response['project'] as Project;
                     }));
