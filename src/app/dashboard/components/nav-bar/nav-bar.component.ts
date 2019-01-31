82985965cec3ade46bd3c544d052834a37560726
stoneLeaf <owi.stoneleaf@gmail.com>
2019-01-31 23:14:11 +0100

    Insert account panel in dashboard navigation bar

--- src/app/dashboard/components/nav-bar/nav-bar.component.ts
+++ src/app/dashboard/components/nav-bar/nav-bar.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthService } from 'src/app/services/auth.service';
-import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
-  user: User;
-
-  constructor(private router: Router,
-              private authService: AuthService) {
-    this.user = authService.loggedInUser;
-  }
-
-  logOut() {
-    this.authService.clearToken();
-    this.router.navigate(['log_in']);
-  }
-}
+export class NavBarComponent { }
