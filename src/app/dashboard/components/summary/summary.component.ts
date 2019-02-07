bfa0d3e0dad0efa7834f4ea3506ecc57f597be9e
stoneLeaf <owi.stoneleaf@gmail.com>
2019-02-07 17:05:13 +0100

    Implementing last 7 days activity chart in dashboard summary page

--- src/app/dashboard/components/summary/summary.component.ts
+++ src/app/dashboard/components/summary/summary.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { StatService } from 'src/app/services/stat.service';
+import { ProjectStat } from 'src/app/shared/models/project-stat.model';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent { }
+export class SummaryComponent implements OnInit {
+  last7DaysData$: Observable<any[]>;
+
+  constructor(private statService: StatService) { }
+
+  ngOnInit() {
+    const endDay = new Date();
+    const startDay = new Date(new Date().setDate(endDay.getDate() - 6));
+    this.last7DaysData$ = this.statService.getForAllProjects(startDay, endDay).pipe(
+      switchMap((globalStats: ProjectStat) => {
+        const single = [];
+        for (const day of globalStats.days) {
+          single.push({ name: new Date(day.day).toLocaleDateString(), value: (day.timeCount / 3600) });
+        }
+        return of(single);
+    }));
+  }
+}
