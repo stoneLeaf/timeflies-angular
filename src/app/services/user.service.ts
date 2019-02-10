db3625490d9b6c2c3a06f11cccf4fbad43dbfff3
stoneLeaf <owi.stoneleaf@gmail.com>
2019-02-10 21:58:23 +0100

    Implement update method to user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
 
 import { environment } from 'src/environments/environment';
 import { User } from 'src/app/shared/models/user.model';
@@ -12,12 +14,24 @@ import { User } from 'src/app/shared/models/user.model';
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private authService: AuthService) { }
 
   create(user: User): Observable<User> {
     return this.http.post(`${environment.apiUrl}/users`, user)
                     .pipe(map(response => {
                       return { profile: response['profile'] } as User;
+                    }));
+  }
+
+  update(user: User): Observable<User> {
+    return this.http.patch(`${environment.apiUrl}/profile`, user)
+                    .pipe(
+                      tap(response => {
+                        this.authService.processNewToken(response['token']);
+                      }),
+                      map(response => {
+                        return { profile: response['profile'] } as User;
                     }));
   }
 }
