1274bfd2e91e1666438bdd9b5a9727cf4f4876c4
stoneLeaf <owi.stoneleaf@gmail.com>
2019-02-10 13:14:20 +0100

    Switch project edit form to reactive approach

--- src/app/dashboard/components/project-edit/project-edit.component.ts
+++ src/app/dashboard/components/project-edit/project-edit.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Observable, throwError } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
+import { switchMap, catchError, tap } from 'rxjs/operators';
+
+import { ToastService } from 'src/app/services/toast.service';
+import { ProjectService } from 'src/app/services/project.service';
 
 import { Project } from 'src/app/shared/models/project.model';
-import { ProjectService } from 'src/app/services/project.service';
 import { NotFoundError } from 'src/app/shared/errors/not-found.error';
-import { ToastService } from 'src/app/services/toast.service';
 import { ValidationError } from 'src/app/shared/errors/validation.error';
 
 @Component({
@@ -17,6 +18,8 @@ import { ValidationError } from 'src/app/shared/errors/validation.error';
   styleUrls: ['./project-edit.component.scss']
 })
 export class ProjectEditComponent implements OnInit {
+  projectForm: FormGroup;
+
   project$: Observable<Project>;
 
   serverValidationError = '';
@@ -25,13 +28,26 @@ export class ProjectEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private router: Router,
+              private formBuilder: FormBuilder,
               private toastService: ToastService,
               private projectService: ProjectService) { }
+
+  get f() {
+    return this.projectForm.controls;
+  }
 
   ngOnInit() {
     this.project$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
         return this.projectService.getById(params.get('id')).pipe(
+          tap(project => {
+            this.projectForm = this.formBuilder.group({
+              'name': [project.name, [Validators.required,
+                                      Validators.minLength(2),
+                                      Validators.maxLength(100)]],
+              'description': [project.description, Validators.maxLength(255)]
+            });
+          }),
           catchError(error => {
             if (error instanceof NotFoundError) {
               // TODO: kinda hacky to navigate to wildcard route
@@ -44,10 +60,10 @@ export class ProjectEditComponent implements OnInit {
     );
   }
 
-  onSubmit(projectForm: NgForm, project: Project) {
+  onSubmit(project: Project) {
     this.submitted = true;
 
-    if (projectForm.invalid) {
+    if (this.projectForm.invalid) {
       return;
     }
 
@@ -56,9 +72,9 @@ export class ProjectEditComponent implements OnInit {
 
     // TODO: if no changes, don't make the back-end call
 
-    this.projectService.update(project)
+    this.projectService.update({...project, ...this.projectForm.value})
       .subscribe((serverProject: Project) => {
-        this.router.navigate(['/projects', project.id]);
+        this.router.navigate(['/projects', serverProject.id]);
         this.toastService.success(`Project '${serverProject.name}' successfully updated!`);
       },
       error => {
